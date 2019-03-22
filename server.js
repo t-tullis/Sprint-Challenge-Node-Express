baf683d462001e6f75bb33f713677cf1109ec930
t-tullis <t.tullis34@gmail.com>
2019-03-22 11:49:29 -0400

    added projects route and completed crud for projects

--- server.js
+++ server.js
@@ -2,8 +2,14 @@ const express = require('express');
 
 const server = express();
 
+//Routers
+const actionsRouter = require('./routers/actionsRouter.js')
+const projectsRouter = require('./routers/projectsRouter.js')
+
 //middleware
 server.use(express.json());
+// server.use('/api/actions', actionsRouter)
+server.use('/api/projects', projectsRouter)
 
 server.get('/', (req, res) => {
     res.send('Sprint-Challenge-Node-Express')
